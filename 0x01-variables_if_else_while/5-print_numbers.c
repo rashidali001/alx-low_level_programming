a07d81b2476a04de95d0a4592358971aa74f9749
rashidali001 <rashidestonian@gmail.com>
2022-07-03 05:54:59 -0700

    f

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	for (int number = 0; number <= 9; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+	return (0);
+}	
