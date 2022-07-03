fd9d88a1455ae8ed07191a91b884e64a75181f68
rashidali001 <rashidestonian@gmail.com>
2022-07-03 06:07:51 -0700

    ff

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int numer;
+
+
+	for (numer = 0; numer <= 9; numer++)
+	{
+		putchar(numer);
+	}
+	putchar("\n");
+	return (0);
+}
