2939dcc546e06b350dfa283621ebf2459322cd9f
rashidali001 <rashidestonian@gmail.com>
2022-07-08 13:03:03 -0700

    g

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int main(void)
+{
+	int max;
+
+	max = a;
+	if (b > max)
+		max = b;
+	if (c > max)
+		max = c;
+	return (max);
+}	
