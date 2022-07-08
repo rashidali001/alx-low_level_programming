da7ea785f45b16904e61cc2eb18e6eddbb6684da
rashidali001 <rashidestonian@gmail.com>
2022-07-07 17:11:21 -0700

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	while (1 < 10)
+	{
+		putchar(i);
+	}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
