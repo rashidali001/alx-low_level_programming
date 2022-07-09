9bd8ac2392bbc4e8a25b83fad028af695996ecf0
rashidali001 <rashidestonian@gmail.com>
2022-07-09 07:50:24 -0700

    0x02

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,8 +9,10 @@
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately
  */
-int _putchar(char c)
-{
- return (write(1, &c, 1));
-}
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
 #endif
