166e0ad7a9585a87ea510f30ab14e304c9dc6016
rashidali001 <rashidestonian@gmail.com>
2022-07-11 19:28:39 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - print 'n' elements of an array
+ * @a: array pointer
+ * @n: integer type
+ * description: numbers must be separated by comma and space
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		prinf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
