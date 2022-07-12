071282040cd0d2033f872c9f71ad4ed7a0b8c4fd
rashidali001 <rashidestonian@gmail.com>
2022-07-11 19:37:47 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string
+ * @dest: char type string
+ * @src: char type string
+ * Description: copy the string pointed to by pointer 'src' to the buffer
+ * pointed to by 'dest'
+ *
+ * Return: pointer to 'dest'
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do{
+		i++;
+		dest[i] = src[i];
+	}while (src[i] != '\0');
+
+	return (dest);
+}
