1d2e3289d50b9b0690c04a15eb2e065741d0ee47
rashidali001 <rashidestonian@gmail.com>
2022-07-11 18:31:39 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * main.h - header file
+ */
+void reset_to_98(int *n);
+
+/**
+ * reset_to_98 - updates value to 98
+ */
+
+
