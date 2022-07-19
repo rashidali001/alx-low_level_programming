6708b6c80fd76076bcab37eb0259988682fbd2ff
rashidali001 <rashidestonian@gmail.com>
2022-07-19 12:21:19 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches
+ * @s: first string
+ * @accept: second string
+ * Return: a pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
