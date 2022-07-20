8dd75aeb7e48d173318ea41f09f806f36adc386d
rashidali001 <rashidestonian@gmail.com>
2022-07-20 16:10:35 -0700

    0x08

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - calculates length os string
+ * @c: pointer argument
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (strlen(s) == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
