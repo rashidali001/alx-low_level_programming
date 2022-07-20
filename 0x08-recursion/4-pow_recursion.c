9f4952b21486c4b117aaca68bd10aa535d3e361f
rashidali001 <rashidestonian@gmail.com>
2022-07-20 16:33:43 -0700

    0x08

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates powers
+ * @x: integer parameter
+ * @y: integer parameter
+ *
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x == 1)
+		return (1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1);
+}	
