c23c7d2df513079ab4734d55c39cbb62f0e4dd67
rashidali001 <rashidestonian@gmail.com>
2022-07-20 17:20:25 -0700

    0x08

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _this_is_root - obtainign root
+ * @n: integer
+ * @is_root: check if done
+ *
+ * Return: integer
+ */
+int _this_is_root(int n, int is_root)
+{
+	if (is_root * is_root > n)
+		return (-1);
+	if (is_root * is_root == n)
+		return (is_root);
+
+	return (_this_is_root(n, is_root + 1));
+}
+
+/**
+ * __sqrt_recursion - call this is root fn
+ * @n: integer
+ *
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1)
+
+	return (_this_is_root(n, 1);
+}
+
