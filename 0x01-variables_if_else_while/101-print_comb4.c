d0d2b1beb15990888cc070c233d74049019ae3ca
rashidali001 <rashidestonian@gmail.com>
2022-08-07 10:38:30 -0700

    if else

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: integer
+ */
+int main(void)
+{
+	int first = 0, second = 0, third = 0;
+	for (first = 0; first <= 9; first ++)
+	{
+		for (second = first + 1; second <= 9; second++)
+		{
+			for (third = second + 1; third <= 9; third++)
+			{
+				if (third == first || third == second)
+					continue;
+			putchar(first + '0');
+			putchar(second + '0');
+			putchar(third + '0');
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+}
