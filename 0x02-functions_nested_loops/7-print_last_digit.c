6f1b7ff118cd8e5f39ba88694ea03e761c941646
rashidali001 <rashidestonian@gmail.com>
2022-08-07 11:43:19 -0700

    0x02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@ int print_last_digit(int para)
 {
 	if (para < 0)
 	{
-		para = 0 - para;
-		_putchar(para + '0');
+		para *= -1;
+		_putchar((para % 10) + '0');
 		return (para % 10);
 	}
-	_putchar(para + '0');
+	_putchar((para % 10) + '0');
 	return (para % 10);
 }
 
