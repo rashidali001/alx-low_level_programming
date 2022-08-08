7d041eb79de3d1232f9cacf8134628b670c188a2
rashidali001 <rashidestonian@gmail.com>
2022-08-07 18:33:10 -0700

    0x0a

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument parameter
+ * @argv: argument vector
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(atoi(argv[i])))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+
+	return (0);
+}
