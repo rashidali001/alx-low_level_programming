098a8a0ee2904ee28a79078415df2bf162d018bb
rashidali001 <rashidestonian@gmail.com>
2022-08-07 17:46:23 -0700

    0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: parameter count
+ * @argv: parameter array
+ *
+ * Return: integer
+ */
+int main(int argc, __noerr char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
