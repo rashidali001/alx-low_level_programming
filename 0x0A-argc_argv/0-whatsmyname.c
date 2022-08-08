4f85b4408bb76f240218134356cdb70e763db758
rashidali001 <rashidestonian@gmail.com>
2022-08-07 17:24:27 -0700

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of parameters
+ * @argv: array of parameters
+ *
+ * Return: integer
+ */
+int main(int argc, __noerr char *argv[])
+{
+	printf("%s",argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#if !defined _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define __noerr __attribute__((unused))
+#define END '\0'
+int _putchar(char c);
+#endif
