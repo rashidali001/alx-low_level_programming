f7cf42465b1c59acd91dce446c4d6aaa6d62b9b1
rashidali001 <rashidestonian@gmail.com>
2022-08-09 06:43:08 -0700

    0x0B

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include  "main.h"
+
+/**
+ * _strdup - entry pont
+ * @str: pointer parameter
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int a, b;
+	char *cpd_str;
+
+	if (str == NULL)
+		return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	cpd_str = malloc(sizeof(char) * (a + 1));
+	if (cpd_str == NULL)
+		return (NULL);
+	for (b = 0; b <= a; b++)
+	{
+		cpd_str[b] = str[b];
+	}
+	return (cpd_str);
+}
