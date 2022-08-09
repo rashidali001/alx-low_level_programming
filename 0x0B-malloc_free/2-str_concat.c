d602f10ca114ba4fb12b4757e423ebe65eed3f22
rashidali001 <rashidestonian@gmail.com>
2022-08-09 10:10:02 -0700

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -38,7 +38,7 @@ char *str_concat(char *s1, char *s2)
 
 	int size1 = getsize(s1);
 	int size2 = getsize(s2);
-	strout = malloc(sizeof(char) * (size1 + size2 + 1);
+	strout = malloc(sizeof(char) * (size1 + size2 + 1));
 
 	if (strout == NULL)
 	{
