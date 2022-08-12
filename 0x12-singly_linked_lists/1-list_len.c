dc50656abdc51fdda450e7e75983c4f68389b317
rashidali001 <rashidestonian@gmail.com>
2022-08-11 19:01:04 -0700

    linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * list_len - entry point
+ * @h: linked list
+ *
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	long unsigned int count  = 0;
+	
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
