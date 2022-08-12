25340e48bdcf0b9702b56e233f382a3896f46519
rashidali001 <rashidestonian@gmail.com>
2022-08-11 19:29:51 -0700

    o

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *
+ *  *free_list - function to free list
+ *
+ *   *@head: a pointer to a head
+ *
+ *    *
+ *
+ *     *Return: Void
+ *
+ *      */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (head)
+	{
+		tmp = head;
+		head = tmp->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
