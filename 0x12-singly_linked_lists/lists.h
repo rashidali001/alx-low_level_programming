162a5714368468f430de56788938fdc9dacd01d7
rashidali001 <rashidestonian@gmail.com>
2022-08-11 19:55:34 -0700

    o

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,13 +15,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int putchar(char);
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void myStartupFun(void) __attribute__ ((constructor));
-void myConstructor(void);
 
 #endif
