a35f58201949cc93bfef5ccacccbb4c2e1ce447a
rashidali001 <rashidestonian@gmail.com>
2022-08-11 20:48:08 -0700

    linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ *
+ *  *myStartupFun - function to print before main function
+ *
+ *   *
+ *
+ *    *Return: Void
+ *
+ *     */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
