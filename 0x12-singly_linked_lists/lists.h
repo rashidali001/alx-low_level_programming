1f5228e7b9bc9e1c5da53d2332d6c49826fad93a
rashidali001 <rashidestonian@gmail.com>
2022-08-11 19:33:23 -0700

    o

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,19 @@
-#ifndef _MAIN_H
+#ifndef _LIST_H
+#define _LIST_H
+
+#define NIL "(nil)"
+#define END '\0'
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int putchar(char);
 size_t print_list(const list_t *h);
@@ -6,5 +21,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void myStartupFun(void) __attribute__ ((constructor));
+void myConstructor(void);
 
 #endif
