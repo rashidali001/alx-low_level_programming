0d165f8ee488836d1a5b8879e20250e697f3c621
rashidali001 <rashidestonian@gmail.com>
2022-08-11 19:11:10 -0700

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,16 @@
  * */
 size_t print_list(const list_t *h)
 {
-	long unsigned int count = 0;
+	unsigned int count = 0;
 
 	while(h != NULL)
 	{
-		if(h -> str != NULL)
-		{
-			printf("[%u] %s\n", h -> len, h -> str);
-		}
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
 		else
-		{
 			printf("[%u] %s\n", 0, NIL);
-		}
-		h = h -> next;
+
+		h = h->next;
 		count++;
 	}
 	return (count);
