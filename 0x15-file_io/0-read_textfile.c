c3fdc3ade0760623c35f0def30453561334a7ed3
rashidali001 <rashidestonian@gmail.com>
2022-08-25 01:05:16 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,35 +9,37 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-	int fd, size;
-	char *buf = malloc(sizeof(char) * letters);
-	size = 0;
+	int fd, m;
+	long int n;
+	char *buf;
+
+	buf = malloc(sizeof(char) * (letters + 1));
+
+	if (buf == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
-	
+
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
-	
-	read(fd, buf, letters);
 
-	buf[letters - 1] = '\0';
-
-	while (buf[i] != '\0')
-	{
-		size++;
-		i++;
-	}
+	n = read(fd, buf, letters);
 
 	close(fd);
 
-	printf("%s\n", buf);
+	if (n == -1)
+		return (0);
 
-	return size;
+	buf[letters] = '\0';
+
+	m = write(STDOUT_FILENO, buf, n)
+	free(buf);
+
+	if (m == -1)
+		return (0);
+
+	return (n);
 }
-	
-
-
-
