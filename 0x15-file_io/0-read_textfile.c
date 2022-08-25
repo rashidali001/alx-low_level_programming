53bfff1fe77c6444ccb353da5e07283a1e82e1f7
rashidali001 <rashidestonian@gmail.com>
2022-08-25 05:39:21 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  *
  * Return: actual number of letters it could read and print
  */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, m;
 	long int n;
