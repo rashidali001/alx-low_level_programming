6b927bcf232360db536163e1bfb1049e8a38320f
rashidali001 <rashidestonian@gmail.com>
2022-10-03 03:27:35 -0700

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int add(int number_1, int number_2)
+{
+	return number_1 + number_2;
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int mod(int number_1, int number_2)
+{
+	return number_1 / number_2;
+}
